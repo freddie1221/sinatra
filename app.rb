4b4a80ba9f9ee9a52f4a07b293340a1a3920da7d
Freddie <fredjlawson@gmail.com>
2018-08-20 17:33:37 +0100

    added parameters for name and age, read by the HTML

--- app.rb
+++ app.rb
@@ -8,7 +8,14 @@ get('/apple') { 'Here is yet another page'}
 get('/pear') { 'Today is monday'}
 get('/orange') { 'Tomorrow is Tuesday'}
 get('/blueberry') { 'The day after tomorrow is Wednesday'}
-get('/cat') {erb :index}
-get('/cat2') {"<div style='border: 3px dashed red'>
-  <img src='http://bit.ly/1eze8aE'>
-</div>"}
+get('/random-cat') {
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
+  }
+  get('/named-cat') {
+
+    p params
+    @name = params[:name]
+    @age = params[:age]
+    erb :index
+    }
